26855d625a083dc026cfc920a8276156bb97dc5d
NursKanyb <1704.01043@manas.edu.kg>
2022-04-26 10:14:57 +0300

    make it better

--- src/main/java/com/any/Any/Controller/AdminPageC.java
+++ src/main/java/com/any/Any/Controller/AdminPageC.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 @Controller
 public class AdminPageC {
 
@@ -18,7 +24,16 @@ public class AdminPageC {
                         @RequestParam(defaultValue = "") String checkbox,@RequestParam(defaultValue = "") String checkbox_1,
                         @RequestParam(defaultValue = "") String email,@RequestParam(defaultValue = "") String text,
                         @RequestParam(defaultValue = "") String text_1) {
-        System.out.println(name);
+        try {
+            URL url = new URL("https://api.telegram.org/bot5331629555:AAFE618hOlG8gqxPrVkejSfA9m2tmAt8ksM/sendMessage?chat_id=636117694&text=" +
+                    name + " " + select__ + " " + checkbox+ " " + checkbox_1+ " " + email+ " " + text+ " " + text_1);
+            URLConnection conn = url.openConnection();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "adminPage";
     }
 
